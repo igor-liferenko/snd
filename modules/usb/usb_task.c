a5ba2b289590a9f9f7c44ed56c2befa7f761bdf2
Igor Liferenko <igor.liferenko@gmail.com>
2018-09-07 11:58:12 +0700

    objdump is equal - now configure

--- modules/usb/usb_task.c
+++ modules/usb/usb_task.c
@@ -454,7 +454,7 @@ void usb_task(void)
 
 
 
-            (PLLFRQ &= ~ ( (1<<PDIV3)| (1<<PDIV2) | (1<<PDIV1)| (1<<PDIV0)) ,PLLFRQ|= ( (1<<PDIV3)| (0<<PDIV2) | (1<<PDIV1)| (0<<PDIV0)) | (1<<PLLUSB) , PLLCSR = ( ( 0<<PINDIV ) | (1<<PLLE))) ;
+            (PLLFRQ &= ~ ( (1<<PDIV3)| (1<<PDIV2) | (1<<PDIV1)| (1<<PDIV0)) ,PLLFRQ|= ( (0<<PDIV3)| (1<<PDIV2) | (0<<PDIV1)| (0<<PDIV0)) | (0<<PLLUSB) , PLLCSR = ( ( 1<<PINDIV ) | (1<<PLLE))) ;
 
          while (!(PLLCSR & (1<<PLOCK))) ;
       }
